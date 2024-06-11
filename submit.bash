e651c40c1683b70d1df39c07ea0526a33b281e14
Dante <notapirate69yarhar@gmail.com>
2024-06-11 08:38:51 +0000

    setup for final
    changed to classmpifinal

--- submit.bash
+++ submit.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -J AutoCorr
 #SBATCH -A cs475-575
-#SBATCH -p classmpitest
+#SBATCH -p classmpifinal
 #SBATCH -N 8 # number of nodes
 #SBATCH -n 8 # number of tasks
 #SBATCH -o mpiproject.out
