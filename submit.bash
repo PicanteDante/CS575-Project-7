5e3004557ad709d28b8cba18f2e1dd598fec335a
Connor Olsen <olsenco@submit-c.ib.coehpc>
2024-06-11 21:14:15 -0700

    finished running

--- submit.bash
+++ submit.bash
@@ -3,11 +3,11 @@
 #SBATCH -A cs475-575
 #SBATCH -p classmpifinal
 #SBATCH -N 8 # number of nodes
-#SBATCH -n 3 # number of tasks
+#SBATCH -n 4 # number of tasks
 #SBATCH -o mpiproject.out
 #SBATCH -e mpiproject.err
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=olsenco@oregonstate.edu
 module load openmpi
 mpic++ main.cpp -o mpiproject -lm
-mpiexec --hostfile hostfile -mca btl self,tcp -np 3 ./mpiproject
+mpiexec --hostfile hostfile -mca btl self,tcp -np 4 ./mpiproject
